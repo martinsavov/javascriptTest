19ca6c2df927921c32d9b3331a55ff4ca1b580b9
git config --global user.name John <martinsavov@ymail.com>
2020-01-09 17:25:01 +0200

    Refresh

--- myFunction.js
+++ myFunction.js
@@ -8,9 +8,20 @@ year(); */
 /* 
 let a = 'node-js';
 console.log(a.split('o').pop()); */
+//Reverse string
+/* function firstLast(str1){
 
-function solve(){
- console.log('test1');
+  let midChar = str1.substring(1, str1.length - 1);
+  return(str1.charAt(str1.length - 1) + midChar + str1.charAt(0));
 }
-solve();
+console.log(firstLast("Hello"));
+ */
+/* 
+function solve(str1){
 
+let last3 = str1.substring(str1.length - 3, str1.length);
+let newString = last3 + str1 + last3;
+console.log(newString);
+
+}
+console.log(solve("Hello")); */
